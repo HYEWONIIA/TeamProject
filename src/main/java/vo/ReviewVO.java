560763bb689a20394e8fab0fcd335cdd1c24bc7c
HyeWonii <isgo6320@naver.com>
2021-09-18 14:16:01 +0900

    09-18

--- src/main/java/vo/ReviewVO.java
+++ src/main/java/vo/ReviewVO.java
@@ -1,5 +1,7 @@
 package vo;
 
+import org.springframework.web.multipart.MultipartFile;
+
 import lombok.Data;
 
 @Data
@@ -11,6 +13,7 @@ public class ReviewVO {
 	private String brdate;
 	private int brcnt;
 	private String brfile;
+	private MultipartFile uploadfilef;
 	private int brating;
 	private String brcity;
 	private int cno;
